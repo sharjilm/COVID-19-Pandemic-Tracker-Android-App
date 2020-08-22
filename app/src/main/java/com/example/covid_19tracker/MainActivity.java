0e695b68ff10270e6de6b1bf46747b2ccf1f5a96
Sharjil M <sharjilmohsin@gmail.com>
2020-08-22 18:23:56 -0400

    Add files via upload

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.covid_19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,21 +16,21 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Set;
 
-public class MainActivity
-        extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    // Create the object of TextView
-    TextView tvCases, tvRecovered,
-            tvCritical, tvActive,
-            tvTodayCases, tvTotalDeaths,
-            tvTodayDeaths,
-            tvAffectedCountries;
+    TextView tvCases, tvRecovered, tvCritical, tvActive, tvTodayCases, tvTotalDeaths, tvTodayDeaths, tvAffectedCountries;
+    SimpleArcLoader simpleArcLoader;
+    ScrollView scrollView;
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,102 +38,75 @@ public class MainActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Link those objects with their respective id's that we have given in .XML file
-        tvCases
-                = findViewById(R.id.tvCases);
-        tvRecovered
-                = findViewById(R.id.tvRecovered);
-        tvCritical
-                = findViewById(R.id.tvCritical);
-        tvActive
-                = findViewById(R.id.tvActive);
-        tvTodayCases
-                = findViewById(R.id.tvTodayCases);
-        tvTotalDeaths
-                = findViewById(R.id.tvTotalDeaths);
-        tvTodayDeaths
-                = findViewById(R.id.tvTodayDeaths);
-        tvAffectedCountries
-                = findViewById(R.id.tvAffectedCountries);
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
 
-        // Creating a method fetchdata()
-        fetchdata();
+        simpleArcLoader = findViewById(R.id.loader);
+        scrollView = findViewById(R.id.scrollStats);
+        pieChart = findViewById(R.id.piechart);
+
+        fetchData();
     }
 
-    private void fetchdata()
-    {
+    private void fetchData() {
+        String url = "https://corona.lmao.ninja/v2/all/";
 
-        // Create a String request using Volley Library
-        String url = "https://corona.lmao.ninja/v2/all";
+        simpleArcLoader.start();
 
-        StringRequest request
-                = new StringRequest(
-                Request.Method.GET,
-                url,
+        StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
-                    public void onResponse(String response)
-                    {
-
-                        // Handle the JSON object and handle it inside try and catch
+                    public void onResponse(String response) {
                         try {
+                            JSONObject jsonObject = new JSONObject(response.toString());
 
-                            // Creating object of JSONObject
-                            JSONObject jsonObject
-                                    = new JSONObject(
-                                    response.toString());
+                            tvCases.setText(jsonObject.getString("cases"));
+                            tvRecovered.setText(jsonObject.getString("recovered"));
+                            tvCritical.setText(jsonObject.getString("critical"));
+                            tvActive.setText(jsonObject.getString("active"));
+                            tvTodayCases.setText(jsonObject.getString("todayCases"));
+                            tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                            tvTodayDeaths.setText(jsonObject.getString("todayDeaths"));
+                            tvAffectedCountries.setText(jsonObject.getString("affectedCountries"));
 
-                            /*
-                             * Set the data in text view
-                             * which are available in JSON format
-                             * Note that the parameter inside
-                             * the getString() must match
-                             * with the name given in JSON format
-                             */
-                            tvCases.setText(
-                                    jsonObject.getString(
-                                            "cases"));
-                            tvRecovered.setText(
-                                    jsonObject.getString(
-                                            "recovered"));
-                            tvCritical.setText(
-                                    jsonObject.getString(
-                                            "critical"));
-                            tvActive.setText(
-                                    jsonObject.getString(
-                                            "active"));
-                            tvTodayCases.setText(
-                                    jsonObject.getString(
-                                            "todayCases"));
-                            tvTotalDeaths.setText(
-                                    jsonObject.getString(
-                                            "deaths"));
-                            tvTodayDeaths.setText(
-                                    jsonObject.getString(
-                                            "todayDeaths"));
-                            tvAffectedCountries.setText(
-                                    jsonObject.getString(
-                                            "affectedCountries"));
+                            pieChart.addPieSlice(new PieModel("Active", Integer.parseInt(tvActive.getText().toString()), Color.parseColor("#29B6F6")));
+                            pieChart.addPieSlice(new PieModel("Deaths", Integer.parseInt(tvTotalDeaths.getText().toString()), Color.parseColor("#EF5350")));
+                            pieChart.addPieSlice(new PieModel("Recovered", Integer.parseInt(tvRecovered.getText().toString()), Color.parseColor("#66BB6A")));
+                            pieChart.startAnimation();
+
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
                         }
+
                         catch (JSONException e) {
                             e.printStackTrace();
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
                         }
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error)
-                    {
-                        Toast.makeText(
-                                MainActivity.this,
-                                error.getMessage(),
-                                Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                });
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                scrollView.setVisibility(View.VISIBLE);
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        RequestQueue requestQueue
-                = Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(request);
+    }
+
+    public void goTrackCountries(View view) {
+        startActivity(new Intent(getApplicationContext(), AffectedCountries.class));
     }
 }
